84c14aff6ab7d85884637ef56d332e8cdd4cbe66
Miguel <miguelsoaresabs7@gmail.com>
2026-06-04 14:34:13 -0300

    init: adição das atividades

---
+++ ex000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Teste</title>
+</head>
+<body>
+  <h1>
+    <?php 
+      echo "Testando";
+    ?>
+  </h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut cupiditate sapiente perferendis neque provident ut dicta commodi, facilis corporis suscipit aperiam nihil veniam placeat cum. Accusamus, id quidem! Dolorem, minima.</p>
+</body>
+</html>
---
+++ ex001/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Teste 2</title>
+</head>
+<body>
+  <?php
+    /* 
+    <?php  ?> O MAIS USADO DE FORMA GERAL (super tag PHP)
+    <? ?> DEPENDENDO DO CÓDIGO, O PHP NÃO RECONHECE (short open tag)
+    <% %> DELIMITAR TAG EM ASP, MAS TEM QUE CONFIGURAR (ASP TAG)
+    <?=  ?> GERALMENTE QUANDO TEM APENAS UM ÚNICO COMANDODENTRODATAG (short tag PHP) 
+    */
+    $nome = "Miguel";
+    echo "<h1>Teste $nome</h1>"
+  ?>
+  <?= "<h2>Teste</h2>" ?>
+</body>
+</html>
