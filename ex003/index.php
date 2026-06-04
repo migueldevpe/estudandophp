5d8bd9aa6a78f97b3e02af70b29896a4ee0432a9
Miguel <miguelsoaresabs7@gmail.com>
2026-06-04 15:05:36 -0300

    new: novos exercícios

---
+++ ex003/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Teste</title>
+</head>
+<body>
+  <?php 
+    $numero = (float) 9; // PARA REALIZAR A DEFINIÇÃO/TIPAGEM DE UMA VARIÁVEL, BASTA COLOCAR UM PARENTESES APÓS O RECEBE: $variavel = (float) 9
+    var_dump($numero); // var_dump($variavel) SERVE PARA MOSTRAR O TIPO DO ELEMENTO QUE ESTÁ GUARDADO NA VARIÁVEL
+  ?>
+</body>
+</html>
